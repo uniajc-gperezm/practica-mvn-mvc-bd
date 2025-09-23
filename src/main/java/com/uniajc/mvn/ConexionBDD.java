92904eb07964b25a1e0d11f7e16e2f3bcb173446
AlejaRivera11 <ariveram@estudiante.uniajc.edu.co>
2025-09-22 20:24:38 -0500

    Se hace correcion

--- src/main/java/com/uniajc/mvn/ConexionBDD.java
+++ src/main/java/com/uniajc/mvn/ConexionBDD.java
@@ -5,11 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBDD {
-    private static final String URL = "jdbc:mysql://localhost/estudiante";
+    private static final String URL = "";
     private static final String USER = "";       
     private static final String PASSWORD = ""; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
+   
 }
