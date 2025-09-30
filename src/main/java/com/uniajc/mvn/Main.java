cc04807757033fdb830edb651f8d00fe1af857d0
AlejaRivera11 <ariveram@estudiante.uniajc.edu.co>
2025-09-30 17:59:00 -0500

    se mueve el menu que estaba en el Main a la carpeta de Vista.

--- src/main/java/com/uniajc/mvn/Main.java
+++ src/main/java/com/uniajc/mvn/Main.java
@@ -13,71 +13,10 @@ public class Main {
 
         Connection conexion = ConexionBDD.getConnection();
 
-        Scanner sc = new Scanner(System.in);
         VistaEstudiante vista = new VistaEstudiante();
         ControladorEstudiante controlador = new ControladorEstudiante(new Estudiante("", 0), vista);
-
-        int opcion;
-    do {
-        System.out.println("\n===== MENÚ GESTION DE ESTUDIANTES =====");
         
-        System.out.println("1. Agregar estudiante");
-        System.out.println("2. Listar todos los estudiantes");
-        System.out.println("3. Eliminar estudiante");
-        System.out.println("4. Actualizar datos del estudiante");
-        System.out.println("5. Salir del programa");
-        
-        System.out.print("Seleccione una de las opción que desea realizar: ");
-        opcion = sc.nextInt();
-        sc.nextLine(); 
-
-        switch (opcion) {
-            case 1:
-             
-                System.out.print("Ingrese el nombre del estudiante: ");
-                String nombre = sc.nextLine();
-
-                System.out.print("Ingrese la edad del estudiante: ");
-                int edad = sc.nextInt();
-                sc.nextLine(); 
-
-                Estudiante estudiante = new Estudiante(nombre, edad);
-                controlador.agregarEstudiante(estudiante);
-                break;
-
-            case 2:
-                controlador.actualizarVista();
-                break;
-            case 3:
-                System.out.print("Ingrese el nombre del estudiante a eliminar: ");
-                String nombreEliminar = sc.nextLine();
-                controlador.removerEstudiante(nombreEliminar);
-                System.out.println("El estudiante ha sido eliminado de la BDD.");
-                break;
-
-            case 4: 
-                System.out.print("Ingrese el nombre del estudiante a actualizar: ");
-                String nombreActualizar = sc.nextLine();
-
-                System.out.print("Ingrese la nueva edad del estudiante: ");
-                int nuevaEdad = sc.nextInt();
-                sc.nextLine(); 
-
-                Estudiante estudianteActualizado = new Estudiante(nombreActualizar, nuevaEdad);
-                controlador.actualizarDatosEstudiante(estudianteActualizado);
-                System.out.println("Los datos del estudiante han sido actualizados en la BDD.");
-                break;
-
-            case 5:
-                System.out.println("Saliendo del programa...");
-                break;
-
-            default:
-                System.out.println("Opción no válida. Verifica !!");
-        }
-
-    } while (opcion != 5);
-
-    sc.close();
-    }
+        vista.menu(controlador);
+          
+     }
 }
--- src/main/java/com/uniajc/mvn/vista/VistaEstudiante.java
+++ src/main/java/com/uniajc/mvn/vista/VistaEstudiante.java
@@ -1,14 +1,83 @@
 package com.uniajc.mvn.vista;
 import com.uniajc.mvn.modelo.Estudiante;
-
+import com.uniajc.mvn.controlador.ControladorEstudiante;
 import java.util.List;
+import java.util.Scanner;
 
 public class VistaEstudiante {
     public void mostrarDetallesEstudiante(List<Estudiante> estudiantes) {
       
-
         estudiantes.forEach(estudiante -> {
             System.out.println("Nombre: " + estudiante.getNombre() + " - " + "Edad: " + estudiante.getEdad());
         });
     }
-}
+
+    public void menu (ControladorEstudiante controlador){
+        Scanner sc = new Scanner(System.in);
+       int opcion;
+    do {
+        System.out.println("\n -------- MENÚ GESTION DE ESTUDIANTES ------");
+        
+        System.out.println("1. Agregar estudiante");
+        System.out.println("2. Listar todos los estudiantes");
+        System.out.println("3. Eliminar estudiante");
+        System.out.println("4. Actualizar datos del estudiante");
+        System.out.println("5. Salir del programa");
+        
+        System.out.print("Seleccione una de las opción que desea realizar: ");
+        opcion = sc.nextInt();
+        sc.nextLine(); 
+
+        switch (opcion) {
+            case 1:
+             
+                System.out.print("Ingrese el nombre del estudiante: ");
+                String nombre = sc.nextLine();
+
+                System.out.print("Ingrese la edad del estudiante: ");
+                int edad = sc.nextInt();
+                sc.nextLine(); 
+
+                Estudiante estudiante = new Estudiante(nombre, edad);
+                controlador.agregarEstudiante(estudiante);
+                break;
+
+            case 2:
+                controlador.actualizarVista();
+                break;
+            case 3:
+                System.out.print("Ingrese el nombre del estudiante a eliminar: ");
+                String nombreEliminar = sc.nextLine();
+                controlador.removerEstudiante(nombreEliminar);
+                System.out.println("El estudiante ha sido eliminado de la BDD.");
+                break;
+
+            case 4: 
+                System.out.print("Ingrese el nombre del estudiante a actualizar: ");
+                String nombreActualizar = sc.nextLine();
+
+                System.out.print("Ingrese la nueva edad del estudiante: ");
+                int nuevaEdad = sc.nextInt();
+                sc.nextLine(); 
+
+                Estudiante estudianteActualizado = new Estudiante(nombreActualizar, nuevaEdad);
+                controlador.actualizarDatosEstudiante(estudianteActualizado);
+                System.out.println("Los datos del estudiante han sido actualizados en la BDD.");
+                break;
+
+            case 5:
+                System.out.println("Saliendo del programa...");
+                break;
+
+            default:
+                System.out.println("Opción no válida. Verifica !!");
+        }
+
+    } while (opcion != 5);
+
+    sc.close();
+    }
+    
+    
+    }
+
